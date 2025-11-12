042d3f0debfddcb8d9fa14c97740a81c5136728c
David.R <david.rodriguez114@educa.madrid.org>
2025-11-12 10:40:11 +0100

    Add CRUD methods to UsuarioDAO interface

--- EjercicioDAO/src/main/java/dao/UsuarioDAO.java
+++ EjercicioDAO/src/main/java/dao/UsuarioDAO.java
@@ -1,4 +1,9 @@
 package dao;
 
 public interface UsuarioDAO {
+    void addUsuario(Libro libro) throws Exception;
+    List<Libro> getAllUsuarios() throws Exception;
+    Libro getUsuarioById(int id) throws Exception;
+    void updateUsuario(Libro libro) throws Exception;
+    void deleteUsuario(int id) throws Exception;
 }
