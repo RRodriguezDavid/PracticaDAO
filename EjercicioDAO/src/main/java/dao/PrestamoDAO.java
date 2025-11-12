51a474d1c014ce81e389c93eef2ce44164059312
David.R <david.rodriguez114@educa.madrid.org>
2025-11-12 10:39:47 +0100

    Add CRUD methods to PrestamoDAO interface

--- EjercicioDAO/src/main/java/dao/PrestamoDAO.java
+++ EjercicioDAO/src/main/java/dao/PrestamoDAO.java
@@ -1,4 +1,9 @@
 package dao;
 
 public interface PrestamoDAO {
+    void addPrestamo(Libro libro) throws Exception;
+    List<Libro> getAllPrtesamos() throws Exception;
+    Libro getPrestamoById(int id) throws Exception;
+    void updatePrestamo(Libro libro) throws Exception;
+    void deletePrestamo(int id) throws Exception;
 }
