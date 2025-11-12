ccc55830cd1040c6d65394aff07266f8ec9fcf89
David.R <david.rodriguez114@educa.madrid.org>
2025-11-12 10:39:15 +0100

    Add CRUD methods to AutorDAO interface

--- EjercicioDAO/src/main/java/dao/AutorDAO.java
+++ EjercicioDAO/src/main/java/dao/AutorDAO.java
@@ -1,4 +1,9 @@
 package dao;
 
 public interface AutorDAO {
+    void addAutor(Libro libro) throws Exception;
+    List<Libro> getAllAutores() throws Exception;
+    Libro getAutorById(int id) throws Exception;
+    void updateAutor(Libro libro) throws Exception;
+    void deleteAutor(int id) throws Exception;
 }
